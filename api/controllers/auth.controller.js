7119ed1a7d07567750c6d6b0cd2be6ae906189db
Mateus Varnier <mateuschioquetta@gmail.com>
2023-10-02 16:35:43 -0300

    create a middleware and a function possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
+import { errorHandler } from "../utils/error.js";
 import User from "./../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const singnup = async (req, res) => {
+export const singnup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -9,6 +10,6 @@ export const singnup = async (req, res) => {
     await newUser.save();
     res.status(201).json("User criado com susesso");
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error);
   }
 };
